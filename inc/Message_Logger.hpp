e8b6ac04720840294a0a0e0d6e6791e325d4c667
FHKarim <furqanhameedkarim63@gmail.com>
2024-09-19 15:40:01 +0500

    Cmake configured Library

---
+++ inc/Message_Logger.hpp
@@ -0,0 +1,14 @@
+#include<fstream>
+#include<iostream>
+#include<string>
+
+class Log_stream{
+    private:
+    std::fstream file;
+    std::string single_log;
+    int maxFileSize;
+    std::string fileName;
+
+    public:
+
+};
